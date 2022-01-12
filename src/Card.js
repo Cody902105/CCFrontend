456489dffcbaacccbc84bccddc0702be41f1448d
Cody902105 <cody.gaydon@gmail.com>
2022-01-12 09:24:38 +0000

    Remove "Add Owned" Functionality

--- src/Card.js
+++ src/Card.js
@@ -1,32 +1,12 @@
 import React,{ useState }from "react";
 import style from "./card.module.css";
 
-const Card = ({imgID,StartOwned,name,rarity,SetCode,cardType,otherSide,price}) =>{
+const Card = ({imgID,name,rarity,SetCode,cardType,otherSide,price}) =>{
     const BASE_URL = "http://localhost:8080/card/addOwned?uuid="+name+"&";
-    const [owned, setOwned] = useState(StartOwned);
     const imgUrl = "https://api.scryfall.com/cards/"+ imgID +"?format=image";
     const [imageState, setImageState] = useState(imgUrl);
     var imgUrlOtherSide = null;
     var CURR_PRICE = 0;
-    async function changeOwn(bool){
-        var FULL_URL = "";
-        var result = "";
-        if(bool){
-            FULL_URL = BASE_URL + "add=123";
-            result = await fetch(FULL_URL);
-            const data = await result.json();
-            console.log(data);
-            setOwned(owned + 1);
-        }else{
-            if (owned - 1 >= 0){
-                FULL_URL = BASE_URL + "remove=123";
-                result = await fetch(FULL_URL);
-                const data = await result.json();
-                console.log(data);
-                setOwned(owned - 1);
-            }
-        }
-    }
     const changeImage = e => {
         if(otherSide !== undefined){
             if(imageState === imgUrl){
@@ -43,9 +23,6 @@ const Card = ({imgID,StartOwned,name,rarity,SetCode,cardType,otherSide,price}) =
         CURR_PRICE = 0;
     }else{
         CURR_PRICE = price;
-    }
-    if (owned === undefined){
-        setOwned(0);
     }
     var CARDTYPE = "";
     if (cardType === undefined){
@@ -77,14 +54,7 @@ const Card = ({imgID,StartOwned,name,rarity,SetCode,cardType,otherSide,price}) =
             <img src={imageState} alt="" className={style.picture} onClick={changeImage}/>
             <div className={style.bar}>
                 <i className={typeIcon}></i> 
-                <button onClick={() => changeOwn(false)}>
-                    -
-                </button>
-                <span>{"Owned: "+ owned}</span>
                 <span>{"£ " + CURR_PRICE}</span>
-                <button onClick={() => changeOwn(true)}>
-                    +
-                </button>
                 <i className={setIcon}></i>
             </div>
         </div>
